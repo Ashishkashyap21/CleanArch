db586054f2c4fbbf236b0af45a78718e818db49b
Ashish Kashyap <akashyap@muniweb.com>
2020-09-28 15:15:09 +0530

    Implementation of course service and repository

---
+++ CleanArch/CleanArch.Application/Interfaces/CourseService.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Interfaces
+{
+    public class CourseService : ICourseService
+    {
+        private ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public CourseViewModel GetCources()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
